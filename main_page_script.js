87cade0bbc8a4315defc577f2738b6c7dbf195e1
Vladimir <79530187+Vovursik@users.noreply.github.com>
2022-03-20 23:40:56 +0300

    Add files via upload

---
+++ main_page_script.js
@@ -0,0 +1,61 @@
+if (localStorage.getItem('theme') == 1) {
+    document.getElementById("body").style.background='url(images/Background.png) no-repeat';
+    document.getElementById("body").style.backgroundColor='black';
+    document.getElementById("body").style.backgroundSize='auto 100vh';
+    document.getElementById("body").style.backgroundAttachment='fixed';
+    document.getElementById("color_button").style.background='url(images/outline_invert_black.png) no-repeat center';
+    document.getElementById("color_button").style.backgroundSize='auto 80%';
+    document.querySelectorAll('div.product_info').forEach((elem) => {
+        elem.style.color='gainsboro';
+      });
+} 
+else
+{
+localStorage.setItem('theme', 0);
+}
+
+if(localStorage.getItem('infShow') != 1) {
+    localStorage.setItem('infShow', 1);
+    document.getElementById("information").style.visibility='visible';
+}
+
+function change_theme() {
+    if (localStorage.getItem('theme') == 0) {
+        localStorage.setItem('theme', 1);
+        document.getElementById("body").style.background='url(images/Background.png) no-repeat';
+        document.getElementById("body").style.backgroundColor='black';
+        document.getElementById("body").style.backgroundSize='auto 100vh';
+        document.getElementById("body").style.backgroundAttachment='fixed';
+        document.getElementById("color_button").style.background='url(images/outline_invert_black.png) no-repeat center';
+        document.getElementById("color_button").style.backgroundSize='auto 80%';
+        document.querySelectorAll('div.product_info').forEach((elem) => {
+            elem.style.color='gainsboro';
+          });
+    } 
+    else {
+        localStorage.setItem('theme', 0);
+        document.getElementById("body").style.background='url(images/Background1.png) no-repeat';
+        document.getElementById("body").style.backgroundColor='white';
+        document.getElementById("body").style.backgroundSize='auto 100vh';
+        document.getElementById("body").style.backgroundAttachment='fixed';
+        document.getElementById("color_button").style.background='url(images/outline_invert_white.png) no-repeat center';
+        document.getElementById("color_button").style.backgroundSize='auto 80%';
+        document.querySelectorAll('div.product_info').forEach((elem) => {
+            elem.style.color='black';
+          });
+    }
+}
+
+window.addEventListener('DOMContentLoaded', function(){ 
+    var buttons = document.getElementsByTagName('button'); 
+    for (var i = 0; i < buttons.length; i++) { 
+        buttons[i].addEventListener('click', function(e){
+            if (e.target.getAttribute('id') == "color_button")
+            change_theme();
+            if (e.target.getAttribute('id') == "information_close_button")
+            document.getElementById("information").style.visibility='hidden';
+            if (e.target.getAttribute('id') == "help_button")
+            document.getElementById("information").style.visibility='visible';
+        }) 
+    } 
+    });
